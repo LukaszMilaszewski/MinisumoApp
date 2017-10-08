88d473e959053231b43189c0a59a3f5afd054027
John Doe <lukasz.milaszewski@gmail.com>
2017-10-08 15:45:45 +0200

    landscape in gyroscope

--- MinisumoApp/MinisumoApp/GyroscopeViewController.swift
+++ MinisumoApp/MinisumoApp/GyroscopeViewController.swift
@@ -1,6 +1,69 @@
 import UIKit
 import CoreBluetooth
 
-class GyroscopeViewController: UIViewController {
+class GyroscopeViewController: UIViewController, BluetoothSerialDelegate {
+
+  @IBOutlet weak var stateButton: UIButton!
+  @IBOutlet weak var powerLabel: UILabel!
+  @IBOutlet weak var directionLabel: UILabel!
+//  var isPresented = true
+  var msg = Array(repeating: "0", count: 10)
+  let ms = 1000
+  var ifStart = true
   
+  @IBAction func gyroscope() {
+    if ifStart {
+      stateButton.setTitle("STOP", for: .normal)
+    } else {
+      stateButton.setTitle("START", for: .normal)
+    }
+    
+    ifStart = !ifStart
+  }
+  
+  override var shouldAutorotate: Bool {
+    return false
+  }
+  
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    return .landscapeRight
+  }
+  
+  override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+    return .landscapeRight
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    ifStart = true
+
+    stateButton.setTitle("START", for: .normal)
+    
+    serial.delegate = self
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(true)
+//    if (self.isMovingFromParentViewController) {
+//      UIDevice.current.setValue(Int(UIInterfaceOrientation.portrait.rawValue), forKey: "orientation")
+//    }
+    print("view disappeard")
+    msg[0] = "0"
+//    serial.sendMessageToDevice(msg.joined())
+  }
+  
+
+  //MARK:- BluetoothSerialDelegate
+  
+  func serialDidReceiveString(_ message: String) {
+    print("received message: \(message)")
+  }
+  
+  func serialDidChangeState() {
+    if serial.centralManager.state != .poweredOn {
+    }
+  }
+  
+  func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
+  }
 }
--- MinisumoApp/MinisumoApp/MainViewController.swift
+++ MinisumoApp/MinisumoApp/MainViewController.swift
@@ -7,6 +7,10 @@ class MainViewController: UIViewController {
     navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Disconnect", style: .plain, target: self, action: #selector(disconnectDevice))
   }
   
+  @IBAction func showGyroscope() {
+    
+  }
+  
   func disconnectDevice() {
     serial.disconnect()
     performSegue(withIdentifier: "DisconnectMain", sender: nil)
